4dbde9294108a2637fb4c89685c187e5d6d8f139
dushyantdawas <dushyantdawas03@gmail.com>
2025-12-24 20:48:53 +0530

    User status UI with online offline indicator

---
+++ script.js
@@ -0,0 +1,5 @@
+// status toggle example
+setTimeout(() => {
+  document.getElementById("statusText").innerText = "Last seen";
+  document.getElementById("statusText").style.color = "gray";
+}, 4000);
